1e886b71030c498429c45fad88ea0c86a65320b7
Beheer account Marcel van Heyningen <marcel@vanheyningen.nl>
2016-06-28 15:06:04 +0200

    test2

--- WindowsFormsApplication1/Program.cs
+++ WindowsFormsApplication1/Program.cs
@@ -14,6 +14,7 @@ namespace WindowsFormsApplication1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            MessageBox.Show("Test");
             Application.Run(new Form1());
         }
     }
